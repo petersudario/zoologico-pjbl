53d7e1829a1f8b22ba523595b6da5b5d9d2f2291
kyoulau <laucosan05@gmail.com>
2023-10-05 19:29:20 -0300

    classe Pessoa

---
+++ src/Pessoa.java
@@ -0,0 +1,12 @@
+public abstract class Pessoa {
+    private String nome;
+    private int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public abstract void trabalhar();
+
+}
