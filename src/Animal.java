696513bcb6c793134871ce045c784e45e5084e8b
1Haku <134961331+haku769@users.noreply.github.com>
2023-10-05 18:38:05 -0300

    classe animal

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,19 @@
-public class Animal {
+public abstract class animal{
+    private String nome;
+    private int idade;
+    private String especie;
+
+    public abstract void fazer_barulho();{
+
+    }
+    public abstract void comer(String alimento);{
+
+    }
+    public abstract void andar();{
+
+    }
+    public abstract void dormir();{
+
+    }
 }
+
