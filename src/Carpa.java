dfcec0e673ba4cf4bf640a605906024ceedd6581
Haku769 <arubacafebrix766@gmail.com>
2023-10-19 19:59:09 -0300

    criação da classe Peixe

---
+++ src/Carpa.java
@@ -0,0 +1,8 @@
+public class Carpa extends Peixes{
+    String tonalidade;
+    Boolean nadadeiras;
+    Boolean escamas;
+    public Carpa(String nome, int idade, String especie) {
+        super(nome, idade, especie);
+    }
+}
---
+++ src/Dourado.java
@@ -0,0 +1,11 @@
+public class Dourado extends  Peixes{
+    Boolean escamas;
+    Boolean nadadeiras;
+    String corDaEscama;
+
+    public Dourado(String nome, int idade, String especie) {
+        super(nome, idade, especie);
+    }
+
+}
+
