21e6d6f6a8962145c29f95e56cf717e97f9e7dd3
Haku769 <arubacafebrix766@gmail.com>
2023-10-19 20:37:45 -0300

    alteração da classe Aguia e salamandra

---
+++ src/Salamandra.java
@@ -0,0 +1,6 @@
+public class Salamandra extends Anfibios{
+    Boolean corpoAlongado;
+    public Salamandra(String nome, int idade, String especie, String tipoPele, Boolean calda) {
+        super(nome, idade, especie, tipoPele, calda);
+    }
+}
