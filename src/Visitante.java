9a91d0498015bd4c4e952a5046f9d88979305df8
camargo.marques <camargo.marques@pucpr.edu.br>
2023-10-26 20:51:29 -0300

    Ajustes de método

--- src/Visitante.java
+++ src/Visitante.java
@@ -12,5 +12,13 @@ public class Visitante extends Pessoa {
     @Override
     void trabalhar() {
         // Implement the behavior for working, if needed
+    }void tirarFoto(){
+        System.out.printf("Visitante $s está tirando foto dos animais !",nome);
+    }
+    void darComida(){
+        System.out.printf("O visitante %s está dando petiscos para os animais !",nome);
+    }
+    void seAlimentar(){
+        System.out.printf("O visitante %s tirou uma pausa pra se alimentar", nome);
     }
 }
