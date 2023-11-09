724e86f19393a148a29fc6cbfa9be06a693f1f70
laura.costa <laucosan05@gmail.com>
2023-11-09 19:44:17 -0300

    Criação arraylist Zoologico

--- src/Zoologico.java
+++ src/Zoologico.java
@@ -1,15 +1,29 @@
+import java.util.ArrayList;
+
 public class Zoologico {
     private String nome;
     private String endereco;
     Jaula jaula;
     Lagos lagos;
+    protected ArrayList<Mamifero> HabitMamifero;
 
     public Zoologico(String nome, String endereco, Jaula jaula, Lagos lagos) {
         this.nome = nome;
         this.endereco = endereco;
         this.jaula = jaula;
         this.lagos = lagos;
+        HabitMamifero = new ArrayList<>();
     }
+
+    public void adicionarHabitat(Animal animal){
+        HabitMamifero.add((Mamifero) animal);
+    }
+
+    public int size(){
+        return HabitMamifero.size();
+    }
+
+
     public void abrir(){
         System.out.println("Zoológico aberto");
     }
